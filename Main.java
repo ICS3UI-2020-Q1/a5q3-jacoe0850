a31ee0daf48be5bf719a0e33574a10f6e3645c6f
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-07 04:50:03 +0000

    Finished factorial

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *shows the factorial of a number
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,18 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //make scanner for input
+    Scanner input = new Scanner(System.in);
+    // ask for ineger and make its variable
+    System.out.println("Please enter an integer to calculate the factorial of");
+    int num = input.nextInt();
+    // answer variable
+    int factorial = 1;
+    // for loop for calculation
+    for(int count=1; count<=num; count++){
+      factorial = count*factorial;
+  }
+  // declare answer
+  System.out.println(num + "! =" + " " + factorial);
   }
 }
